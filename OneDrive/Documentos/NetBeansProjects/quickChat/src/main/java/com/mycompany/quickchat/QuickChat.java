80e287eb3bdb136f8060ad9298f7ac0fa3651138
Siyabonga <ST10534389@rcconnect.edu.za>
2026-04-13 12:50:27 +0200

    Creating a userRegistrationAndLogin class

--- OneDrive/Documentos/NetBeansProjects/quickChat/src/main/java/com/mycompany/quickchat/QuickChat.java
+++ OneDrive/Documentos/NetBeansProjects/quickChat/src/main/java/com/mycompany/quickchat/QuickChat.java
@@ -1,5 +1,21 @@
 package com.mycompany.quickchat;
 
+//Importing the scanner to prompt the user
+import java.util.*;
+
+
+//creating a template or blueprint of a chatting app using a class and methods
+class userRegistrationAndLogin
+{
+    //Creating a scanner object on a global scanner so that any method that wants to access the scanner in this class can
+    Scanner userInput = new Scanner(System.in);
+    
+    //Chatting app attributes
+    String storeduserName;
+    String storedpassword;
+    String storedcellPhoneNumber;
+    boolean isLoggedIn = false;
+
 public class QuickChat {
 
     public static void main(String[] args) {
